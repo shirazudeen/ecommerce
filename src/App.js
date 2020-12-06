8af59bbfa79fb5f4dbf2e7d6a512451eacad9dd6
bokya <67274133+shirazudeen@users.noreply.github.com>
2020-12-07 03:22:59 +0530

    link added

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import './App.css';
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <h1 style={{textAlign:'center',color:'red'}}>super duper app</h1>
+      <h1 style={{ textAlign: "center", color: "red" }}>Winston Churchill</h1>
+      <p style={{ textAlign: "center", color: "green" }}>Success is not final, failure is not fatal: it is the courage to continue that counts.</p>
       
+
+
+      <h1 style={{ textAlign: "center", color: "red" }}>Jimmy Dean</h1>
+      <p style={{ textAlign: "center", color: "green" }}>
+I can't change the direction of the wind, but I can adjust my sails to always reach my destination.</p>
+      <h1 style={{ textAlign: "center", color: "red" }}>Demi Lovato</h1>
+      <p style={{ textAlign: "center", color: "green" }}>
+No matter what you're going through, there's a light at the end of the tunnel.</p>
+      
+
     </div>
   );
 }
